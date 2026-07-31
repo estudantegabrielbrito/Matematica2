9aed284eb03299f746e86054f0fdd08da13c65f2
estudantegabrielbrito <gabriel.brito21@escola.pr.gov.br>
2026-07-31 11:48:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+if (2 > 3){
+    console.log("O número é maior!")
+}else{
+    console.log("O número é menor!")
+}
+for(let i = 0; i <5; i++){
+    console.log(i)
+}
+let num1 = 4;
+let num2 = 2;
 
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
